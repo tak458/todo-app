43fc5cdb70bae9b3cb71b8aa673a637e49d68394
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-01 16:52:49 +0000

    Deploy tak458/todo-app to github.com/tak458/todo-app.git:gh-pages

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -5,10 +5,7 @@ export const mutations = {
     const _addTodo = (index, todos, { id, title }) => {
       console.log({ index, todos, id, title })
       if (index.length() === 1) {
-        todos.push({
-          id,
-          title
-        })
+        todos.push({ id, title })
       } else {
         const newIndex = index.excludeRootId().toString()
         if (todos.find(todo => new Id(todo.id).foot() === index.head()).children === undefined)
@@ -18,7 +15,16 @@ export const mutations = {
     }
     _addTodo(new Id(id), state.todos, { id, title })
   },
-  removeTodo(state, { index }) {
-    state.todos.splice(index, 1)
+  removeTodo(state, { id }) {
+    const _removeTodo = (index, todos, { id }) => {
+      console.log({ index, todos, id })
+      if (index.length() === 1) {
+        todos.splice(todos.findIndex(todo => todo.id === id), 1)
+      } else {
+        const newIndex = index.excludeRootId().toString()
+        _removeTodo(new Id(newIndex), todos.find(todo => new Id(todo.id).foot() === index.head()).children, { id })
+      }
+    }
+    _removeTodo(new Id(id), state.todos, { id })
   }
 }
