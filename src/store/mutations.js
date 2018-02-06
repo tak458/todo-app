f6e48ec13798eb6e5c37c12943b2520fc7c83eac
tak458 <takah458@gmail.com>
2018-02-07 01:20:09 +0900

    オブジェクト単位でバインドするように修正

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,30 +1,30 @@
 import Id from "./Id"
 
 export const mutations = {
-  addTodo(state, { id, title }) {
-    const _addTodo = (index, todos, { id, title }) => {
-      console.log({ index, todos, id, title })
+  addTodo(state, model) {
+    const _addTodo = (index, todos, model) => {
+      console.log({ index, todos, model })
       if (index.length() === 1) {
-        todos.push({ id, title })
+        todos.push(model)
       } else {
         const newIndex = index.excludeRootId().toString()
         if (todos.find(todo => new Id(todo.id).foot() === index.head()).children === undefined)
           todos.find(todo => new Id(todo.id).foot() === index.head()).children = []
-        _addTodo(new Id(newIndex), todos.find(todo => new Id(todo.id).foot() === index.head()).children, { id, title })
+        _addTodo(new Id(newIndex), todos.find(todo => new Id(todo.id).foot() === index.head()).children, model)
       }
     }
-    _addTodo(new Id(id), state.todos, { id, title })
+    _addTodo(new Id(model.id), state.todos, model)
   },
-  removeTodo(state, { id }) {
-    const _removeTodo = (index, todos, { id }) => {
-      console.log({ index, todos, id })
+  removeTodo(state, model) {
+    const _removeTodo = (index, todos, model) => {
+      console.log({ index, todos, model })
       if (index.length() === 1) {
-        todos.splice(todos.findIndex(todo => todo.id === id), 1)
+        todos.splice(todos.findIndex(todo => todo.id === model.id), 1)
       } else {
         const newIndex = index.excludeRootId().toString()
-        _removeTodo(new Id(newIndex), todos.find(todo => new Id(todo.id).foot() === index.head()).children, { id })
+        _removeTodo(new Id(newIndex), todos.find(todo => new Id(todo.id).foot() === index.head()).children, model)
       }
     }
-    _removeTodo(new Id(id), state.todos, { id })
+    _removeTodo(new Id(model.id), state.todos, model)
   }
 }
