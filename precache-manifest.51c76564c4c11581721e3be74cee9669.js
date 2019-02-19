eb445ae81e223cc04ba8e35738f899881ed130d1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-19 18:00:39 +0000

    Deploy tak458/todo-app to github.com/tak458/todo-app.git:gh-pages

---
+++ precache-manifest.51c76564c4c11581721e3be74cee9669.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b8072d048164a090f1e",
+    "url": "/todo-app/js/chunk-vendors.d8391507.js"
+  },
+  {
+    "revision": "38eedf1912dbd1abd008",
+    "url": "/todo-app/js/app.48baa779.js"
+  },
+  {
+    "revision": "6d43335f5994317a6060",
+    "url": "/todo-app/js/about.4b07f9a9.js"
+  },
+  {
+    "revision": "3f1fa9a9566c5dbd8f817600d12d8631",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "9b8072d048164a090f1e",
+    "url": "/todo-app/css/chunk-vendors.b3458765.css"
+  }
+];
