ed2c460ff5b9e3a34e2d8ca2d0d5aebc8b5aa991
tak458 <takah458@gmail.com>
2021-10-09 01:00:00 +0900

    [WIP]他プロジェクトに構造を合わせる

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
-  root: true,
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "next", "next/core-web-vitals", "prettier"],
+  plugins: ["jest"],
+  env: {
+    "jest/globals": true,
+  },
   rules: {
+    "no-redeclare": "off",
     "react/prop-types": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-empty-interface": "off",
   },
 };
