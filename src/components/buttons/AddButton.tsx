b6eb07092ed9aa4113506604f21e9f694fef944b
tak458 <takah458@gmail.com>
2022-05-07 18:39:06 +0900

    Revert "ダミーのエラーを入れる"
    This reverts commit 729220ffa7346f8a4eb02d29da0af5ac8dcc9215.

--- src/components/buttons/AddButton.tsx
+++ src/components/buttons/AddButton.tsx
@@ -2,8 +2,6 @@ import { IconButton, IconButtonProps } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import React, { FC } from "react";
 
-import Hoge from "hoge";
-
 export const AddButton: FC<IconButtonProps> = (props) => {
   return (
     <IconButton {...props} size="large">
--- src/components/buttons/DeleteButton.tsx
+++ src/components/buttons/DeleteButton.tsx
@@ -2,10 +2,7 @@ import { IconButton, IconButtonProps } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import React, { FC } from "react";
 
-import Hoge from "hoge";
-import Fuga from "fuga";
-
-export const DeleteButton: FC<IconButtonProps> = (props: any) => {
+export const DeleteButton: FC<IconButtonProps> = (props) => {
   return (
     <IconButton {...props} size="large">
       <DeleteIcon />
