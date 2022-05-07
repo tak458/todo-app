729220ffa7346f8a4eb02d29da0af5ac8dcc9215
tak458 <takah458@gmail.com>
2022-05-07 18:30:41 +0900

    ダミーのエラーを入れる

--- src/components/buttons/DeleteButton.tsx
+++ src/components/buttons/DeleteButton.tsx
@@ -2,7 +2,10 @@ import { IconButton, IconButtonProps } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import React, { FC } from "react";
 
-export const DeleteButton: FC<IconButtonProps> = (props) => {
+import Hoge from "hoge";
+import Fuga from "fuga";
+
+export const DeleteButton: FC<IconButtonProps> = (props: any) => {
   return (
     <IconButton {...props} size="large">
       <DeleteIcon />
--- src/components/buttons/EditButton.tsx
+++ src/components/buttons/EditButton.tsx
@@ -2,6 +2,8 @@ import { IconButton, IconButtonProps } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import React, { FC } from "react";
 
+export interface TestProps extends Pick<IconButtonProps, "colors"> {}
+
 export const EditButton: FC<IconButtonProps> = (props) => {
   return (
     <IconButton {...props} size="large">
