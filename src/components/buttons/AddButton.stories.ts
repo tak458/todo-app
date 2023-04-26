1444d4f326ebad1521c93829f6d09f018a04a265
tak458 <takah458@gmail.com>
2023-04-27 00:24:54 +0900

    ボタンのストーリーを適当に追加

---
+++ src/components/buttons/AddButton.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { AddButton } from "./AddButton";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof AddButton> = {
+  title: "buttons/AddButton",
+  component: AddButton,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof AddButton>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    color: "primary",
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Secondary: Story = {
+  args: {
+    color: "secondary",
+  },
+};
---
+++ src/components/buttons/DeleteButton.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { DeleteButton } from "./DeleteButton";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof DeleteButton> = {
+  title: "buttons/DeleteButton",
+  component: DeleteButton,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof DeleteButton>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    color: "primary",
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Secondary: Story = {
+  args: {
+    color: "secondary",
+  },
+};
