1b02047695e867c452c767ed6ddb33cfb1a8cffc
tak458 <takah458@gmail.com>
2023-04-26 23:40:49 +0900

    storybook追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,12 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "next", "next/core-web-vitals", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next",
+    "next/core-web-vitals",
+    "prettier",
+    "plugin:storybook/recommended",
+  ],
   plugins: ["jest"],
   env: {
     "jest/globals": true,
