1bb03a408de2aa79b73e99798828f434679b9ae1
tak458 <takah458@gmail.com>
2023-05-05 00:31:19 +0900

    story 追加

---
+++ src/components/TaskAddDialog.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { TaskAddDialog } from "./TaskAddDialog";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+const MockStore = ({ children }: any) => (
+  <Provider
+    store={configureStore({
+      reducer: {},
+    })}
+  >
+    {children}
+  </Provider>
+);
+
+const meta: Meta<typeof TaskAddDialog> = {
+  title: "global/TaskAddDialog",
+  component: TaskAddDialog,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof TaskAddDialog>;
+
+export const Main: Story = {
+  decorators: [
+    (story) => {
+      return <MockStore>{story()}</MockStore>;
+    },
+  ],
+};
