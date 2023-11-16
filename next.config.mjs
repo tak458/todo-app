6903c71a5ad6fe96a2e035665370912011f052d5
tak458 <takah458@gmail.com>
2023-11-17 04:16:33 +0900

    next export を config に移動

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,7 @@
 const config = {
   assetPrefix: process.env.NODE_ENV === "production" ? "/todo-app" : undefined,
   reactStrictMode: true,
-  /* If trying out the experimental appDir, comment the i18n config out
-   * @see https://github.com/vercel/next.js/issues/41980 */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  swcMinify: true,
+  output: "export",
 };
 export default config;
