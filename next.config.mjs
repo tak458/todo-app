970ab69e31fec55c5621ebe19a34e4642b39dba6
tak458 <takah458@gmail.com>
2025-02-22 23:11:00 +0900

    remove config params which removed vercel/next.js#73281

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@
 const config = {
   assetPrefix: process.env.NODE_ENV === "production" ? "/todo-app" : undefined,
   reactStrictMode: true,
-  swcMinify: true,
   output: "export",
 };
 export default config;
