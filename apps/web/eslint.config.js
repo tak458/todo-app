2eccb7fd29747ff699495d0831e09dad012acc2d
tak458 <takah458@gmail.com>
2025-05-27 03:28:12 +0900

    eslint, typescript config をパッケージ化

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,26 +1,4 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import vitest from "@vitest/eslint-plugin";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import storybook from "eslint-plugin-storybook";
+import { nextJsConfig } from "@repo/eslint-config/next-js";
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next", "next/core-web-vitals", "next/typescript"],
-  }),
-  eslintConfigPrettier,
-  {
-    plugins: {
-      vitest,
-    },
-    rules: {
-      ...vitest.configs.recommended.rules,
-    },
-  },
-  ...storybook.configs["flat/recommended"],
-];
-
-export default eslintConfig;
+/** @type {import("eslint").Linter.Config} */
+export default nextJsConfig;
