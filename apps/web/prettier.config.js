8cda8d9ee81bec7ff9615fa744cd4ec7fda9a250
tak458 <takah458@gmail.com>
2025-05-27 03:28:12 +0900

    eslint, typescript config をパッケージ化

--- apps/web/prettier.config.js
+++ apps/web/prettier.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-anonymous-default-export */
 /** @type {import('prettier').Config} */
 export default {
   printWidth: 120,
---
+++ packages/eslint-config/base.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import vitest from "@vitest/eslint-plugin";
+import eslintConfigPrettier from "eslint-config-prettier";
+import onlyWarn from "eslint-plugin-only-warn";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
+export const config = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+    },
+  },
+  {
+    plugins: {
+      onlyWarn,
+    },
+  },
+  {
+    ignores: ["dist/**"],
+  },
+  {
+    plugins: { vitest },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+  },
+];
